21639b2ae8080bc1ec5161aaf6a57ad9ed4cb51a
riordan.santos <riordan3607@gmail.com>
2022-05-06 18:43:43 -0300

    feat(): full project

---
+++ client/app.js
@@ -0,0 +1,53 @@
+const CategoryService = require('./services/CategoryService')
+const ProductService = require('./services/ProductService')
+const OrderService = require('./services/OrderService')
+
+const app = socket => {
+  socket.on('data', data => {
+    const listParams = data.toString().trim().split('-|-')
+
+    switch (listParams[0]) {
+      case 'WELCOME':
+        console.log('Seja bem vindo ao mercadinho Mais Você!')
+        socket.write('listCategories')
+        break
+      case 'listAndChooseCategories':
+        const category = CategoryService.listAndChooseCategories(listParams[1])
+        socket.write(`chosenCategory-|-${category}`)
+        break
+      case 'listAndChooseProduct':
+        const userChoice = JSON.stringify(ProductService.listAndChooseProduct(listParams[1]))
+        socket.write(`chosenProduct-|-${userChoice}`)
+        break
+      case 'addAnotherProduct':
+        const userAnswer = ProductService.addAnotherProduct()
+        if (userAnswer === '1') {
+          socket.write('listCategories')
+          break
+        }
+        socket.write('getOrder')
+        break
+      case 'showOrder':
+        OrderService.showOrder(listParams[1])
+        const deleteProduct = OrderService.deleteProduct(listParams[1])
+        if(deleteProduct) {
+          socket.write(`deleteProduct-|-${deleteProduct}`)
+          break
+        }
+        const closeOrder = OrderService.closeOrder()
+        socket.write(`closeOrder-|-${closeOrder}`)
+        break
+      case 'orderClosed':
+        console.log("Pedido finalizado com sucesso! Volte Sempre!")
+        socket.end()
+        break
+      case 'ERROR':
+        console.log(listParams[1])
+        break
+      default:
+        break
+    }
+  })
+};
+
+module.exports = app
---
+++ readme.md
@@ -0,0 +1,35 @@
+**Participantes**: [Gilson Santos](https://github.com/gilsonsantos03) e [Matheus Figueiredo](https://github.com/matheusF23)
+
+# Groceries Socket
+
+Esta aplicação é uma implementação do socket com NodeJS para exemplificar uma comunicação cliente-servidor no cenário de um supermercado.
+
+## Clonar projeto
+
+Use o comando para clonar o projeto em seu computador
+```
+$ git clone git@github.com:matheusF23/groceries-socket.git
+```
+
+## Instalação das dependências
+
+Para instalar as dependências use o comando:
+```
+$ yarn install
+```
+
+## Execução do projeto
+Estando na pasta do projeto é necessário executar o servidor e o cliente. Para executar o servidor utilize:
+```
+$ yarn server
+```
+Para executar o cliente utilize:
+```
+$ yarn client
+```
+
+# Fluxo de execução
+
+O projeto objetiva realizar o seguinte fluxo:
+
+![image](https://user-images.githubusercontent.com/54044801/145727206-000994d0-828f-4166-8769-2a0b08eed8dc.png)
